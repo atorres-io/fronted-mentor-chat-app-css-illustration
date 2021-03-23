4964a591c4e5cad74238b214c62ced01f771a63f
Alejandro Torres <atorresalcala.me@gmail.com>
2021-03-23 11:53:19 +0100

    build(remove): remove unnecesary files

--- sources/app.js
+++
@@ -1,5 +0,0 @@
-const app = new ChatView();
-
-// Initiation listeners
-
-app.initListeners();
--- sources/components/chat.view.js
+++
@@ -1,7 +0,0 @@
-class ChatView {
-	constructor() {
-		this.GUI = {};
-	}
-
-	initListeners = () => {};
-}
